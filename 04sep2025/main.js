9499a8c7ac57d7c50c3a0c081c2900befbd97b06
surajsawant512 <surajsa647@gmail.com>
2025-09-04 11:51:03 +0530

    Add files via upload
    done 4sep normal code

---
+++ 04sep2025/main.js
@@ -0,0 +1,78 @@
+ "use strict"
+
+//+ve -ve number
+
+// let n =+prompt("enter number")
+// if(0<n){
+//     console.log("+ve",n);
+    
+// }
+// else{
+//      console.log("-ve",n);
+
+// }
+
+
+
+
+
+
+
+
+// gretest number between 2 numbers
+
+
+// let n1=+prompt("enter number")
+// let n2=20
+
+// if(n1>n2){
+//     console.log("number is big");
+
+// }
+
+// else{
+//      console.log("number is big");
+
+// }
+
+
+
+
+
+
+// gretest number between 3 numbers
+
+// let n1=10;
+// let n2=20;
+// let n3=30;
+
+// if(n1>n2 && n1>n3){
+//     console.log("n1 is greater");
+// } else if( n2>n1 && n2>n3 ){
+
+//      console.log("n1 is greater");
+// }
+// else{
+
+//      console.log("n1 is greater");
+// }
+
+
+
+
+
+
+
+
+
+// oddeven number cheak
+
+
+// let n1=3;
+
+//     if (n1 % 2 ===0) {
+//         console.log(n1+"is Even");
+//     } else {
+//         console.log(n1 +"is Odd");
+//     }
+

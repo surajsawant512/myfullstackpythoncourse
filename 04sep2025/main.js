fc9bf38027489c8c1c25f4c735768b205149a23f
surajsawant512 <surajsa647@gmail.com>
2025-09-05 12:09:03 +0530

    5th sep 2025
    today we learn javascript control statements, and we done the marks grade check and leap year checker programs

--- 04sep2025/main.js
+++ 04sep2025/main.js
@@ -1,11 +1,11 @@
- "use strict"
+"use strict"
 
 //+ve -ve number
 
 // let n =+prompt("enter number")
 // if(0<n){
 //     console.log("+ve",n);
-    
+
 // }
 // else{
 //      console.log("-ve",n);
@@ -76,3 +76,52 @@
 //         console.log(n1 +"is Odd");
 //     }
 
+
+// leap year program
+
+// let year=+prompt("enter year");
+
+// if (year % 4==0){
+//     console.log("this is leap year");
+
+// }
+// else{
+//     console.log("this is not leap yaer")
+// }
+
+
+// grade check 
+
+let m = +prompt("enter marks");
+
+if (m >= 90) {
+    console.log("A+");
+
+} else if (m >= 80) {
+    console.log("B+");
+}else if(m>=70){
+    console.log("C+");
+
+}else if(m>=60){
+    console.log("D+");
+
+}else if(m>=50){
+    console.log("E+")
+
+}else if(m>=40){ 
+    console.log("F+")
+
+}else if(m>=30){ 
+    console.log("G+")
+
+}else if(m>=20){
+    console.log("H+")
+
+}else if (m>=10){
+    console.log("I+")
+}
+
+
+else {
+    console.log("Fail");
+ }
